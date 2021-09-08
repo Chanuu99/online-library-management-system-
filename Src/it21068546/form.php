c092f599cf6e348f6b5d7607c816887e800f5b49
adithya heshan <jadithya803@gmail.com>
2021-09-08 09:52:22 +0530

    updated

--- Src/it21068546/form.php
+++ Src/it21068546/form.php
@@ -1,6 +1,56 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+<style>
+ ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.active {
+  background-color: #4CAF50;
+}
+.container {
+                 border-radius: 5px;
+                 background-color: #f2f2f2;
+                 padding: 10px 10px 10px 10px;
+                 margin:1% 10% 10% 10% ;
+
+                  }
+h3 {
+  text-shadow: 2px 2px 5px red;
+}
+
+input[type=text], select {
+ 
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+</style>
+
+    <!--Layout -->
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -9,6 +59,14 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">	<title> My Web Page</title>
 </head>
 <body>
+<!--nav -->
+<ul>
+  <li><a href="/">Home</a></li>
+  <li><a href="Src/it21068546/form.php">Calculator</a></li>
+  <li><a href="#">Folder</a></li>
+  <li><a href="/README.md">Contact</a></li>
+  <li style="float:right"><a class="active" href="/README.md">About</a></li>
+</ul>
 
 
 	<!--calculator Form -->
--- index.php
+++ index.php
@@ -1,14 +1,68 @@
 <html>
   <head>
     <title>Dashboard</title>
+    <style>
+        ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.active {
+  background-color: #4CAF50;
+}
+.container {
+                 border-radius: 5px;
+                 background-color: #f2f2f2;
+                 padding: 10px 10px 10px 10px;
+                 margin:1% 10% 10% 10% ;
+
+                  }
+h3 {
+  text-shadow: 2px 2px 5px red;
+}
+
+input[type=text], select {
+ 
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+
+
+
+</style>
   </head>
   <body>
     <?php echo '<p>Welcome to our new app</p>'; ?> 
   </body>
-  <script >
-  var a=">Hello!";
-  let b= prompt("Enter your name:");
-  alert(a+b)
-  </script>
+   <ul>
+  <li><a href="/">Home</a></li>
+  <li><a href="Src/it21068546/form.php">Calculator</a></li>
+  <li><a href="#">Folder</a></li>
+  <li><a href="/README.md">Contact</a></li>
+  <li style="float:right"><a class="active" href="/README.md">About</a></li>
+</ul>
+   
 <a href="Src/it21068546/form.php"><Button >Calculator </Button>
 </a></html>
