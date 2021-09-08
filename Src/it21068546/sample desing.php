b17417300cfedd5e3ee2c027e8d69aeebac6667e
adithya heshan <jadithya803@gmail.com>
2021-09-08 18:30:08 +0530

    sample impliment

---
+++ Src/it21068546/sample desing.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="/CSS/main.css">
+	<!---->
+	<style type="text/css">
+		.container {
+                 border-radius: 5px;
+                 background-color: #f2f2f2;
+                 padding: 10px 10px 10px 10px;
+                 margin:1% 10% 10% 10% ;
+
+                  }
+         .link:hover{
+		color: white;
+		text-shadow: currentColor;
+		background-color: darkblue;
+		text-overflow: inherit;
+		font-style: bold;
+
+
+
+	}
+button:hover{
+		color: white;
+		text-shadow: currentColor;
+		background-color: darkblue;
+		text-overflow: inherit;
+		font-style: bold;
+
+
+
+	}
+	button.active{
+		color:blue;
+	}
+	button{
+		width: 50%;
+		height: 10%;
+		align-items: center;
+		font-size: 120%;
+		border:none;
+		border-radius: 30px;
+	}
+
+	</style>
+	<title>Request Book</title>
+</head>
+<body>
+	<!--1 nav bar-->
+
+	<!--2 nav bar &menu-->
+
+	<!--Request form-->
+	<div class="container">
+		<center><button id="txtName" class="form-control" name="txtName"  value="0">ABC</button><br>
+		<button id="txtName" class="form-control" name="txtName"  value="0">ABC</button><br>
+		<button id="txtName" class="form-control" name="txtName"  value="0">ABC</button><br>
+		<button id="txtName" class="form-control" name="txtName" tvalue="0">ABC</button><br></center>
+		<center><button id="txtName" class="form-control" name="txtName"  value="0">ABC</button><br>
+		<button id="txtName" class="form-control" name="txtName"  value="0">ABC</button><br>
+		<button id="txtName" class="form-control" name="txtName"  value="0">ABC</button><br>
+		<button id="txtName" class="form-control" name="txtName" tvalue="0">ABC</button><br></center>
+		<center><button id="txtName" class="form-control" name="txtName"  value="0">ABC</button><br>
+		<button id="txtName" class="form-control" name="txtName"  value="0">ABC</button><br>
+		<button id="txtName" class="form-control" name="txtName"  value="0">ABC</button><br>
+		<button id="txtName" class="form-control" name="txtName" tvalue="0">ABC</button><br></center>
+   <button><a href="#" class="link">Click Me!</a></button> 
+	</div>
+
+</body>
+</html>
