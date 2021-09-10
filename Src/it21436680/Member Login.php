51b5bcf6434e35ff8146f90f68411a99c8028883
Hasindu-Dilshan <it21436680@my.sliit.lk>
2021-09-10 17:46:30 +0530

    Created Login form (Member Login.php)
    Made the basic format of  Member login page

--- Src/it21436680/Member Login.php
+++ Src/it21436680/Member Login.php
@@ -1,13 +1,56 @@
 <!DOCTYPE html>
+
 <html>
+
 <head>
-	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="/it21436680/style.css">
+	<title>Login Form</title>
 </head>
+	
 <body>
+	<center>
+		<!-- Form -->
+		<form>
+			<fieldset>
+				<h1>Login</h1>
+				<div class="cont_1">
+					<label class="lbl_credentials">Email</label>
+					<div>
+						<label>Need an account?</label>
+						<a  href="/it21436680/regmem_signup_page.php" style="color: #04aa6d;text-decoration: none">Signup</a>
+					</div>
+				</div>
+				
+				<input type="text" class="input_fields">
+				<br><br>
+				
+				<div class="cont_2">
+					<label class="lbl_credentials">Password</label>
+					<div style="background-color: lightgreen;"></div>
+				</div>
+				
+				<input type="password" class="input_fields">
+				<br><br>
 
+				<!--
+				<div class="cont_3">
+					<input type="checkbox">
+					<label type="text">Remember me</label>
+				</div>
+				-->
+				
+				<input type="checkbox">
+				<label type="text">Remember me</label>
+				<br><br>
+				
+				<button>Login</button>
+				<button>Continue as a Guest</button>
+				<br><br>
+				
+			</fieldset>
+		</form>
+		<!-- End of form-->
+	</center>
 </body>
-</html>
 
-<?php
-	echo "Hello PHP";
-?>
+<html>
