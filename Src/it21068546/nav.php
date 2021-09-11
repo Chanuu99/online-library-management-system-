5a853e92129ba94b8f74d6cc357c3d922d02b01a
adithya heshan <jadithya803@gmail.com>
2021-09-11 13:19:57 +0530

    Update nav.php

--- Src/it21068546/nav.php
+++ Src/it21068546/nav.php
@@ -48,7 +48,7 @@
 <style>
 
 .container {
-                 border-radius: 5px;
+                 border-radius: 10rem;
                  background-color: #f2f2f2;
                 
   width: 100%;
