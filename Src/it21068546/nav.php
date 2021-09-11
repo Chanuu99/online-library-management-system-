81c9e54c0807e3bdadbb8b24020f274655395834
adithya heshan <jadithya803@gmail.com>
2021-09-11 12:34:58 +0530

    Update nav.php

--- Src/it21068546/nav.php
+++ Src/it21068546/nav.php
@@ -8,7 +8,7 @@
   <!--Css link CSS/main.css -->
 <link rel="stylesheet" type="text/css" href="online-library-management-system-\CSS">
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">  
+<!--link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous"-->  
 
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
