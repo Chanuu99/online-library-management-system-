7943d25594694f8c3a2e304190ea69b86cd971c4
adithya heshan <jadithya803@gmail.com>
2021-09-12 21:49:49 +0530

    comment add

--- Src/it21068546/Header.php
+++ Src/it21068546/Header.php
@@ -18,51 +18,80 @@
 <HEADER>
 <!--Side nav bar-->
 <div id="mySidenav" class="sidenav">
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <a class="gest" color="white"><i class="fa fa-user-circle fa-2x" style="color:white; text-decoration: none; " aria-hidden="true" ></i>Hello, Sign in</a><br>    
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;
+  </a>
 
+  <!--menu header-->
+  <a class="gest" color="white"><i class="fa fa-user-circle fa-2x" style="color:white; text-decoration: none; " aria-hidden="true" ></i>Hello, Sign in</a><br> 
+
+  <!--menu-->
   <a href="/">🎨 Dashboard</a>
   <a href="Src/it21068546/RequestBooks.php">🕮 Request Book</a>
   <a href="https://adithya-coder.github.io/online-library-management-system-/">🛈  About Us</a>
   <a href="#">👤 Contact</a>
+
+  <!--line-->
   <hr>
+
+  <!--feedback-->
   <div class="rate" >
       <style type="text/css">
             .checked {
-  color: orange;
-}
+                color: orange;
+            }
       </style>
-<h2>Feedback</h2><br>
-<h4>Rate Us</h4>
-<span class="fa fa-star checked"></span>
-<span class="fa fa-star checked"></span>
-<span class="fa fa-star checked"></span>
-<span class="fa fa-star"></span>
-<span class="fa fa-star"></span>
-</div><br>
-<hr>
+      <!--feedback-->
+      <h2>Feedback</h2><br>
+      <h4>Rate Us</h4>
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span>
+  </div><br>
+
+  <!--line-->
+  <hr>
+  <!--text area-->
   <textarea placeholder="">
   
-</textarea><br><button class=" btn btn-primary" style="align-items:right;margin-left: 75%; margin-top:0.5rem;">Submit</button><hr>
+  </textarea><br>
+  <!--submit feedback-->
+  <button class=" btn btn-primary" style="align-items:right;margin-left: 70%; margin-top:0.5rem;">Submit</button>
+
+  <!--line-->
+  <hr>
+
+  <!--feedback-->
   <div class="qr">
       <img src="https://blog.qr1.at/wp-content/uploads/2015/05/qr-code-mulit-appstore.jpg" width="100%" align="center">
-</div>
-
+  </div>
+<!--End Side nav bar-->
 </div>
 
 
 <!--nav bar  logo srach,admin-->
 <div class="topnav">
-
-      <div class="nav-logo"> 
-      <img src="https://th.bing.com/th/id/R.36dd618e18bb5ebe5cb174e97d9c4812?rik=Upd0klNFHi%2fCGw&riu=http%3a%2f%2fwww.antioquiatic.edu.co%2fimages%2flogo-biblioteca.png&ehk=7EicayNy8FNz9p9owaZTAi2BRCphdaTviysnInw2bv4%3d&risl=&pid=ImgRaw&r=0"  > </div>
-  </div>
-
-<!--nav bar  menu and navigation-->
-<div class="topnav1">
-      
-   <span style="color:white;padding-left:1rem;    font-style: unset;    font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Open</span><span style="display: inline-flex; text-overflow:inherit;margin-left: 2rem;  margin-bottom:1rem;   font-weight: bold;"> <a href="./" style="color: #b4b9bb; padding:0px;word-spacing: 0.5rem;">👤User 🞂 🕮 RequestBook</a></span>
+    <!-- logo-->
+    <div class="nav-logo"> 
+      <img src="https://th.bing.com/th/id/R.36dd618e18bb5ebe5cb174e97d9c4812?rik=Upd0klNFHi%2fCGw&riu=http%3a%2f%2fwww.antioquiatic.edu.co%2fimages%2flogo-biblioteca.png&ehk=7EicayNy8FNz9p9owaZTAi2BRCphdaTviysnInw2bv4%3d&risl=&pid=ImgRaw&r=0"  > 
     </div>
+<!-- End nav bar  logo srach,admin-->
+</div>
+
+<!--nav bar  menu countrol and navigation-->
+<div class="topnav1">
+    <!--side nav bar open  l-->
+    <span style="color:white;padding-left:1rem; font-style: unset;    font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Open
+    </span>
+    <!--navigation-->
+    <span style="display: inline-flex; text-overflow:inherit;margin-left:    2rem;  margin-bottom:1rem;   font-weight: bold;"> <a href="./"      style="color: #b4b9bb; padding:0px;word-spacing: 0.5rem;">👤User 🞂 🕮 RequestBook</a>
+    </span>
+<!--End nav bar  menu countrol and navigation-->
+</div>
+
 </HEADER>
+
 </body>
+
 </html>
--- Src/it21068546/RequestBooks.php
+++ Src/it21068546/RequestBooks.php
@@ -1,105 +1,114 @@
 <?php 
+//Header
 include("./Header.php");
  ?>
- <link rel="stylesheet" type="text/css" href="css/request.css">
-<!--Layout -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <!-- linking Javascript -->
-<script type="text/javascript" src="js/nac.js"></script>  
+<script type="text/javascript" src="js/nac.js"></script> 
+
 <!--Icon pack-->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <!--linking css-->
 <link rel="stylesheet" type="text/css" href="css/nav.css">
+<link rel="stylesheet" type="text/css" href="css/request.css">
+
+<!--Page Head-->
 <head>
-  
-
-
-<!--Page title-->
+   <!--Page title-->
    <title>Request Book</title>
 </head>
 
 <!-- Body of page-->
 <body>
-<div id="main">
-<!--RequestBook   Form -->
-<div class="container"  style="background-color: #f2f2f2; margin: 1rem; border-top-right-radius: 20px;
-  border-top-left-radius: 20px";>
+  <!--main-->
+  <div id="main">
+    <!--RequestBook   Form -->
+    <div class="container"  
+        style="background-color: #f2f2f2; margin: 1rem; border-top-right-radius: 20px;
+          border-top-left-radius: 20px";>
 
-<!-- header 5 -->
-<div class="jumbotron">
-    <h3>Book Details</h3>
-</div>
-
-<!--Content-->
-<div class="modal-content">
-
-  <!-- form Body-->
-  <div class="modal-body mb-5 p-5">
-
-    <!--Form  -->
-    <form method="post" action=" form.php" style="box-sizing: border-box;" >
-
-      <!--input field   -->
-      <div class="form-groups">
-      
-        <input id="txtName" class="form-controls"  name="txtName" placeholder="🕮 Book Name" type="text" value=""></input><br>
-      </div>
-
-      <!--input field   -->
-      <div class="form-groups">
-        <input id="txtAge" class="form-controls" name="txtAge" placeholder="👤 Auther" type="Text" value=""></input><br>
-      </div>
-
-      <!--drop-Down  field  -->
-      <div class="form-groups">
-        <select id="txt" class="form-controls" placeholder="Category" name="op">
-          <option>🖹 Category</option>
-          <option>Education</option>
-          <option>Novel</option>
-          <option>Other</option>
-        </select><br>
+    <!-- header 1 -->
+    <div class="jumbotron">
+        <h3>Book Details</h3>
     </div>
 
-     <!--drop-Down Language field  -->
+    <!--Content-->
+    <div class="modal-content">
+
+      <!-- form Body-->
+      <div class="modal-body mb-5 p-5">
+
+        <!--Form  -->
+        <form method="post" action=" form.php" style="box-sizing: border-box;" >
+
+          <!--input field   -->
+          <div class="form-groups">
+          
+            <input id="txtName" class="form-controls"  name="txtName" placeholder="🕮 Book Name" type="text" value=""></input><br>
+          </div>
+
+          <!--input field   -->
+          <div class="form-groups">
+            <input id="txtAge" class="form-controls" name="txtAge" placeholder="👤 Auther" type="Text" value=""></input><br>
+          </div>
+
+          <!--drop-Down  field  -->
+          <div class="form-groups">
+            <select id="txt" class="form-controls" placeholder="Category" name="op">
+              <option>🖹 Category</option>
+              <option>Education</option>
+              <option>Novel</option>
+              <option>Other</option>
+            </select><br>
+        </div>
+
+         <!--drop-Down Language field  -->
+        <div class="form-groups">
+            <select id="txt" class="form-controls"  name="op">
+              <option>🗚 Language</option>
+              <option>English</option>
+              <option>Sinhala</option>
+              <option>Tamil</option>
+            </select><br>
+        </div>
+
+    <!-- header 2 -->
+    <div class="jumbotron " style=" padding: 0.5rem ;border-top-right-radius: 0px;
+      border-top-left-radius: 0px;">
+      <h3>Member Details</h3>
+    </div>
+
     <div class="form-groups">
-        <select id="txt" class="form-controls"  name="op">
-          <option>🗚 Language</option>
-          <option>English</option>
-          <option>Sinhala</option>
-          <option>Tamil</option>
-        </select><br>
+            <!--input field  user details -->
+            <input id="txtName" class="form-controls"  name="txtName" placeholder="👤 Full Name" type="text" value=""></input><br>
+          </div>
+
+          <!--input field  -->
+          <div class="form-groups">
+            <input id="txtAge" class="form-controls" name="txtAge" placeholder="🖂 Email" type="email" value=""></input><br>
+          </div>
+
+         <!--Submit  -->
+        <div class="form-groups">
+            <input id="btnS" class=" btn btn-primary"  class="btn-lg" name="btnS" type="Submit" value="Request" 
+            style="height:4rem; width: 100%; font-size: 2rem;border-radius: 1rem;" >
+            </input>
+        </div>
+
+     </div>
+     <!-- End Form  -->
+     </form>
+    <!-- End form Body-->
     </div>
-    <!-- header 5 -->
-<div class="jumbotron " style=" padding: 0.5rem ;border-top-right-radius: 0px;
-  border-top-left-radius: 0px;">
-    <h3>Member Details</h3>
+    <!--End Content-->
+  </div>
+  <!--end container-->
+  </div>    
+<!--End main-->
 </div>
-<div class="form-groups">
-      
-        <input id="txtName" class="form-controls"  name="txtName" placeholder="👤 Full Name" type="text" value=""></input><br>
-      </div>
-
-      <!--input field   -->
-      <div class="form-groups">
-        <input id="txtAge" class="form-controls" name="txtAge" placeholder="🖂 Email" type="email" value=""></input><br>
-      </div>
- <!--Submit  -->
-    <div class="form-groups">
-
-       <input id="btnS" class=" btn btn-primary"  class="btn-lg" name="btnS" type="Submit" value="Request" style="height:4rem; width: 100%; font-size: 2rem;border-radius: 1rem;" ><style type="text/css"></style></input>
-    </div>
-
-   </div>
-<!-- End Form  -->
-</form>
-<!--end container-->
-
-</div>
-
-</div>
-</dvi>
+</body>
+  
 </body>
 
  <?php 
