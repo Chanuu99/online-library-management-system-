700ec6c9d08b623e5990e1771cae9dd2666a313a
oshanie nimasha <it21007910@my.sliit.lk>
2021-09-12 19:09:16 +0530

    about us,add event,book report

---
+++ Src/it21007910/about us.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>about us </title>
+</head>
+
+<body>
+    <h1><b>ABOUT US</b></h1>
+    <form>
+        <div class="image-upload-wrap">
+            <input class="file-upload-input" type="file" onechange="redURL(this);" accepte="image/*" />
+            <div class="file-upload-content">
+                <img class="file-upload-content">
+                <img class="image-title-wrap" width="200" height="150">
+            </div>
+        </div>
+
+
+        <h3><b>We Provide you...</b></h3>
+
+        <h4>
+            <p>with the ability to view and download <br>many types of books,articales and <br>reports onliner for free</p>
+        </h4>
+
+        <br><br><br><br><br><br><br><br><br><br>
+
+
+
+
+
+
+
+        <input type="button" name="btnl" value="Leran More">
+    </form>
+
+
+
+
+</body>
+
+</html>
---
+++ Src/it21007910/add event.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>add event</title>
+</head>
+
+<body>
+<center><h1>add event</h1>
+<hr>
+    <form>
+        <div>
+		
+            <lable for="Even Id">Event Id:</lable>
+            <input type="text" name="name" id="name" required>
+        </div>
+        <br><br><br>
+        <div class="col-md-4">
+            <lable>Event Date:</lable>
+            <input type="text" placeholder="12 May 2016" id="12 May 2016" />
+
+            <lable>Event Time:</lable>
+            <input type="text" placeholder="12 55 AM" id="12 55 AM" />
+        </div>
+        <br><br><br>
+
+        <div>
+            <lable for="Event name">Event Name:</lable>
+            <input type="text" name="name">
+        </div>
+
+        <br><br><br>
+
+        <div>
+            <lable for="Event Discription">Event Discription:</lable>
+            <textarea name="discription" id="discription" rows="10" cols="30">
+
+            </textarea>
+        </div><br><br>
+
+        <input type="button" name="btnl" value="publish">
+    </form>
+
+
+
+
+</body>
+
+</html>
