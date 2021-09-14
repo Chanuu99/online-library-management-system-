5e162410319faa1a6b007c04faca8cb43ebe6e1f
adithya heshan <jadithya803@gmail.com>
2021-09-14 11:58:22 +0530

    Update RequestBooks.php

--- Src/it21068546/RequestBooks.php
+++ Src/it21068546/RequestBooks.php
@@ -110,71 +110,41 @@ include("./Header.php");
             style="height:4rem; width: 100%; font-size: 2rem;border-radius: 1rem;" >
             </input>
         </div>
+         <!-- End Form  -->
+     </form>
 <div class="viwe" style="display:flex;">
       <table id="customers">
   <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
+    <th>Book</th>
+    <th>Author</th>
+    <th>Stetus</th>
   </tr>
   <tr>
     <td>Alfreds Futterkiste</td>
     <td>Maria Anders</td>
-    <td>Germany</td>
+    <td>✔️</td>
   </tr>
   <tr>
     <td>Berglunds snabbköp</td>
     <td>Christina Berglund</td>
-    <td>Sweden</td>
+    <td>❌</td>
   </tr>
   <tr>
     <td>Centro comercial Moctezuma</td>
     <td>Francisco Chang</td>
-    <td>Mexico</td>
+    <td>✔️</td>
   </tr>
   <tr>
     <td>Ernst Handel</td>
     <td>Roland Mendel</td>
     <td>Austria</td>
   </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Königlich Essen</td>
-    <td>Philip Cramer</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Laughing Bacchus Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td>Canada</td>
-  </tr>
-  <tr>
-    <td>Magazzini Alimentari Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td>Italy</td>
-  </tr>
-  <tr>
-    <td>North/South</td>
-    <td>Simon Crowther</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Paris spécialités</td>
-    <td>Marie Bertrand</td>
-    <td>France</td>
-  </tr>
+ 
 </table>
 
     </div>
      </div>
-     <label style="display:flex;">Heshan</label>
-     <!-- End Form  -->
-     </form>
-     <label style="display: inline-flex;">adithya</label>
+     
      </center>
     <!-- End form Body-->
     
