7cf181f2ddd8c4230acf4e46ab0ace850dd2cdd8
oshanie nimasha <it21007910@my.sliit.lk>
2021-10-03 23:22:49 +0530

    add event,about us,book report

---
+++ Src/it21007910/it21007910/book report.php
@@ -0,0 +1,69 @@
+<?php 
+include("./Header.php");
+ ?>
+
+<!DOCTYPE html>
+<html>
+<title>Book Report</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<link rel="stylesheet" href="bookreport.css">
+<body>
+
+<div class="w3-container">
+  <h1>Book Report</h1>
+
+  <table class="w3-table">
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>User Name</th>
+      <th>Remove</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td> </td>
+    </tr>
+    <tr>
+     <td>2</td>
+      <td>Jacob</td>
+      <td>Thoton</td>
+      <td>@fat</td>
+      <td> </td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Larry</td>
+      <td>the bird</td>
+      <td>@twitter</td>
+      <td> </td>
+    </tr>
+    ​ <tr>
+      <td>4</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td> </td>
+    </tr>
+    ​ <tr>
+      <td>5</td>
+      <td>Jacob</td>
+      <td>Throton</td>
+      <td>@fat</td>
+      <td> </td>
+    </tr>
+    ​ <tr>
+      <td>6</td>
+      <td>Larry</td>
+      <td>the bird</td>
+      <td>@twitter</td>
+      <td> </td>
+    </tr>
+  </table>
+</div>
+
