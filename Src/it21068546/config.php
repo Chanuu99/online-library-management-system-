b6ec36569c95fd74f7c56fa580f6628edbec7e1b
Adithya Heshan <it21068546@my.sliit.lk>
2021-10-05 14:46:59 +0530

    up

--- Src/it21068546/config.php
+++ Src/it21068546/config.php
@@ -1,22 +1,8 @@
 <?php
-/*$servername = "remotemysql.com";
+$servername = "remotemysql.com";
 $username = "CNqQ9qf2VC";
 $password = "VEyeXiBvbd";
 $dbname = "CNqQ9qf2VC";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-*/
-?>
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "online_libaray";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
