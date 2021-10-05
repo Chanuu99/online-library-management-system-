50c8a126d0c30477482fcfd723954fb6ce13f6f4
Adithya Heshan <it21068546@my.sliit.lk>
2021-10-05 14:31:38 +0530

    Update Header.php

--- Src/it21068546/Header.php
+++ Src/it21068546/Header.php
@@ -3,7 +3,7 @@
 
 <!--Layout -->
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
 
 <!-- linking Javascript -->
 <script type="text/javascript" src="js/nac.js"></script>  
