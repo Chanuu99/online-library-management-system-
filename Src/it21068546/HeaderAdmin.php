89711d0442f098852d2adb7003b75d80a64f2615
Adithya Heshan <it21068546@my.sliit.lk>
2021-10-06 18:51:02 +0530

    fix bug

--- Src/it21068546/HeaderAdmin.php
+++ Src/it21068546/HeaderAdmin.php
@@ -32,7 +32,7 @@
     backdrop-filter: blur(25px);">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;
   </a>
-c 
+
   <!--menu header-->
   <a class="gest" color="white"><i class="fa fa-user-circle fa-2x" style="color:white; text-decoration: none; " aria-hidden="true" ></i> Admin <?php /* echo "$username";*/ ?></a><br> 
    <div style="margin-left:0rem; word-spacing: 1px; font-size:1rem; font-family: 'Varela Round';font-weight: bold;";>
