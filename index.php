4658ab171ddbcb5c49d0ba99ae36831430d273f4
Adithya Heshan <it21068546@my.sliit.lk>
2021-10-08 19:06:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,15 +11,7 @@
 		transition: 0.5s;
 		background: #262626;
 	}
-	img
-	{
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		
-	align-items: center;
-		
-	}
+
 	.ring
 	{
 		position: absolute;
