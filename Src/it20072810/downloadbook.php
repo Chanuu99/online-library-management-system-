4e7cb07812911a0e6b22dd0e3a0f1ee31ea27872
Adithya Heshan <it21068546@my.sliit.lk>
2021-10-10 23:42:14 +0530

    Update downloadbook.php

--- Src/it20072810/downloadbook.php
+++ Src/it20072810/downloadbook.php
@@ -2,7 +2,33 @@
 <?php 
 	include("downloadHeader.php");
  ?>
+<?php 
+  
+$ftab="";
 
+ // include database connection file
+    include_once("config.php");
+     /*-------------------------------------book----------------------------------------------*/
+    $fsql = "SELECT ISBN, Book_Name, Cover_pic FROM book ORDER BY ISBN DESC ";
+        $fpp = mysqli_query($conn, $fsql);
+
+ 
+        // output data of each row if(isset($_POST['Request_ID']))
+        if ($fpp->num_rows > 0) {
+            while($rows = $fpp->fetch_assoc()) {
+              $image = $rows['Cover_pic'];    
+                $ftab.=" <div style=' display: grid; grid-template-columns:auto;'>";
+                $ftab.="<div class='card' style=' margin:3% 3% 3% 3%; padding: 3% 3% 3% 3%; '>";
+                $ftab.="<div class='row'> <div class='column'>";
+                $ftab.= "<center><img  class='apps'src='../it20029586/image/$image' width='10%' height='5%'></center><br>";
+               /* $ftab.= "<h2> ". $rows['Book_Name']."</h2></br>";*/
+                $ftab.="<center><a href='details.php?ISBN=$rows[ISBN]'><button  class='btn btn-remove btns nin'> &#128308; Details</center> </div></div>";
+                $ftab.="</div></div>";
+        
+            }
+        }
+
+ ?>
 
 
  
@@ -34,11 +60,32 @@ width: 100%;
 <!--WRITE YOUR CODE HERE-->
 
 
+
+
+ </head>
+ <body bgcolor="black">
+<div style=" display: grid; grid-template-columns: auto auto auto auto auto auto;">
+
+
+  
+    <?php echo "$ftab" ?>
+ </div>
+
+</div>
+
+
+   
+
+</a>
+  
+
+
+</body>
 </html>
-
-
 
 // Do not remove
 <?php
-	include("Footer.php");
+	   include("Src/it21068546/Footer.php");
+
+
 ?>
