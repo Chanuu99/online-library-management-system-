ffd045804a06dc2d44ec90e2980353d4facb66cb
Adithya Heshan <it21068546@my.sliit.lk>
2021-10-10 17:36:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -134,7 +134,23 @@ setTimeout(function(){
 		document.getElementById("ring").onload = function() {src="index1.php"};
 	});
 
-	 
+	 function textToAudio() {
+                let msg = document.getElementById("text-to-speech").value;
+                
+                let speech = new SpeechSynthesisUtterance();
+                speech.lang = "en-US";
+                
+                speech.text = msg;
+                speech.volume = 1;
+                speech.rate = 1;
+                speech.pitch = 1;
+                
+                window.speechSynthesis.speak(speech);
+            }
+
+            function myFunction() {
+  alert('textToAudio()');
+}
 	
 </script>
 </html>
