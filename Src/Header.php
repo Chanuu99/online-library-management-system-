785e355b00c627c82483e01a4a7e367970139808
Adithya Heshan <it21068546@my.sliit.lk>
2021-10-10 23:00:50 +0530

    update

--- Src/Header.php
+++ Src/Header.php
@@ -128,7 +128,7 @@
 
     }
     ?>
-    <form action="" method="post">
+    <form  method="post">
   <textarea placeholder="Write Your.. " name="feed-b" style="height:100%;">
   
   </textarea><br>
