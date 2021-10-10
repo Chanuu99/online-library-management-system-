fb08a690362e2e5553edfa5f3c6e32799f7b1a54
Adithya Heshan <it21068546@my.sliit.lk>
2021-10-10 23:34:02 +0530

    h

--- indexs.php
+++ indexs.php
@@ -50,7 +50,7 @@ width: 100%;
 
 }
     </style>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    
     <script type="text/javascript" src="Src/it21068546/js/nac.js"></script> 
 <link rel="stylesheet" type="text/css" href="Src/it21068546/css/nav.css">
     <title>Dashboard</title>
