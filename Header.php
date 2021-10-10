eae3dd268fd13fe2b724f122dadd8dbe02281173
Adithya Heshan <it21068546@my.sliit.lk>
2021-10-10 23:30:28 +0530

    Update Header.php

--- Header.php
+++ Header.php
@@ -13,11 +13,11 @@
 
 
 <!-- linking Javascript -->
-<script type="text/javascript" src="js/nac.js"></script>  
+<script type="text/javascript" src="Src/it21068546/js/nac.js"></script>  
 <!--Icon pack-->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <!--linking css-->
-<link rel="stylesheet" type="text/css" href="css/nav.css">
+<link rel="stylesheet" type="text/css" href="Src/it21068546/css/nav.css">
 
 
 </head>
