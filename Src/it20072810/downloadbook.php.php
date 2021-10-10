97aae7670eca72511d06590e6d98bc389d6dd7d6
Adithya Heshan <it21068546@my.sliit.lk>
2021-10-10 23:07:33 +0530

    UDATE

--- Src/it20072810/downloadbook.php.php
+++ Src/it20072810/downloadbook.php.php
@@ -1,48 +1,65 @@
 <?php 
-	include("./HeaderAdmin.php");
+  include("Src/Header.php");
 $ftab="";
 
  // include database connection file
     include_once("config.php");
      /*-------------------------------------book----------------------------------------------*/
-		$fsql = "SELECT ISBN, Book_Name, Cover_pic FROM book ORDER BY ISBN DESC ";
+    $fsql = "SELECT ISBN, Book_Name, Cover_pic FROM book ORDER BY ISBN DESC ";
         $fpp = mysqli_query($conn, $fsql);
 
  
         // output data of each row if(isset($_POST['Request_ID']))
         if ($fpp->num_rows > 0) {
             while($rows = $fpp->fetch_assoc()) {
-            	$image = $rows['Cover_pic'];    
-                $ftab.=" <div style=' display: grid; grid-template-columns:auto auto auto  ;'>";
+              $image = $rows['Cover_pic'];    
+                $ftab.=" <div style=' display: grid; grid-template-columns:auto;'>";
                 $ftab.="<div class='card' style=' margin:3% 3% 3% 3%; padding: 3% 3% 3% 3%; '>";
                 $ftab.="<div class='row'> <div class='column'>";
-                $ftab.= "<center><img  class='apps'src=' $image' width='10%' height='5%'></center>";
-                $ftab.= "<center><h2> <center>". $rows['Book_Name']."</h2></br></center>";
-                $ftab.="<center><td><center><a href='details.php?ISBN=$rows[ISBN]'><button  class='btn btn-remove btns'> &#128308; Details</center> </div></div>";
+                $ftab.= "<center><img  class='apps'src='../it20029586/image/$image' width='10%' height='5%'></center><br>";
+               /* $ftab.= "<h2> ". $rows['Book_Name']."</h2></br>";*/
+                $ftab.="<center><a href='details.php?ISBN=$rows[ISBN]'><button  class='btn btn-remove btns nin'> &#128308; Details</center> </div></div>";
                 $ftab.="</div></div>";
-				
+        
             }
         }
 
  ?>
+<html>
+  <head>
+    <style type="text/css">
+       body{
+  
+ 
+bg-blend-mode: overlay;
+background-image: radial-gradient(50% 50% at top center,rgba(0,0,0,.70),#262626), url("https://images.unsplash.com/photo-1570102881689-c04ab4cf1f4c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YmFsbG9vbnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60");;
+   
 
- <!DOCTYPE html>
- <html>
- <head>
+   -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+ }
+ .viwe{
 
-<!-- linking Javascript -->
-<script type="text/javascript" src="js/nac.js"></script> 
+overflow-y:scroll;
+overflow-wrap:300px ;
+height:300px;
+display:inline-grid;
+width: 100%;
 
-<!--Icon pack-->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+}
+    </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <script type="text/javascript" src="Src/it21068546/js/nac.js"></script> 
+<link rel="stylesheet" type="text/css" href="Src/it21068546/css/nav.css">
+    <title>Dashboard</title>
 
-<!--linking css-->
-<link rel="stylesheet" type="text/css" href="css/nav.css">
-<link rel="stylesheet" type="text/css" href="css/request.css">
+
 <link rel="stylesheet" href="AppSty/cate.css"/>
     <script src="AppJS/ctg.js"></script>
- 	<meta charset="utf-8">
- 	<title> </title>
+  <meta charset="utf-8">
+  <title> </title>
 
  <style type="text/css">
  a{
@@ -63,7 +80,10 @@ background-image: radial-gradient(50% 50% at top center,rgba(0,0,0,.70),#262626)
   background-size: cover;
  }
 
+.nin{
+    margin-left: 90%;
 
+}
 
 .viwe{
 
@@ -94,6 +114,9 @@ padding-right:3%;
         margin-bottom: 40px;
     }
  }
+ h2{
+    margin-left:100%;
+ }
  /* card */
  .card{
 
@@ -118,21 +141,23 @@ box-shadow: 20px 20px 50px rgba(0, 0, 0,0.5);
 </style>
  </head>
  <body bgcolor="black">
-<div style=" display: grid; grid-template-columns: auto auto auto auto auto auto ;">
+<div style=" display: grid; grid-template-columns: auto auto auto auto auto auto;">
 
 
-	
- 		<?php echo "$ftab" ?>
+  
+    <?php echo "$ftab" ?>
  </div>
 
 </div>
 
 
+   
 
- </body>
+</a>
+   <?php   include("Src/it21068546/Footer.php");
+   ?>
 
- </html>
- <?php 
-//include footer
- require("./Footer.php")
-  ?>
+
+</footer>
+</body>
+</html>
