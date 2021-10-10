bfa25554a12d7fd2996997fc6b344a58a480251e
Adithya Heshan <it21068546@my.sliit.lk>
2021-10-10 22:53:14 +0530

    Update indexs.php

--- indexs.php
+++ indexs.php
@@ -1,7 +1,28 @@
 <?php 
+  include("Src/Header.php");
+$ftab="";
 
-session_start();
+ // include database connection file
+    include_once("config.php");
+     /*-------------------------------------book----------------------------------------------*/
+    $fsql = "SELECT ISBN, Book_Name, Cover_pic FROM book ORDER BY ISBN DESC ";
+        $fpp = mysqli_query($conn, $fsql);
+
  
+        // output data of each row if(isset($_POST['Request_ID']))
+        if ($fpp->num_rows > 0) {
+            while($rows = $fpp->fetch_assoc()) {
+              $image = $rows['Cover_pic'];    
+                $ftab.=" <div style=' display: grid; grid-template-columns:auto;'>";
+                $ftab.="<div class='card' style=' margin:3% 3% 3% 3%; padding: 3% 3% 3% 3%; '>";
+                $ftab.="<div class='row'> <div class='column'>";
+                $ftab.= "<center><img  class='apps'src='../it20029586/image/$image' width='10%' height='5%'></center>";
+                $ftab.= "<h2> ". $rows['Book_Name']."</h2></br>";
+                $ftab.="<center><a href='details.php?ISBN=$rows[ISBN]'><button style='' class='btn btn-remove btns nin'> &#128308; Details</center> </div></div>";
+                $ftab.="</div></div>";
+        
+            }
+        }
 
  ?>
 <html>
