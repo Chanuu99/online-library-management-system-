e78784d8f4ccbc7ec98b19ef6f9339097db15c72
Hasindu-Dilshan <it21436680@my.sliit.lk>
2021-10-17 20:59:38 +0530

    connection fixed

--- Src/it21436680/publisher/includes/mem_signup.php
+++ Src/it21436680/publisher/includes/mem_signup.php
@@ -1,11 +1,11 @@
 <?php
 	require '../config.php';
 	
-	$First_Name = mysqli_escape_string($con, $_POST['txtFname']);
-	$Last_Name = mysqli_escape_string($con, $_POST['txtLname']);
-	$Email = mysqli_escape_string($con, $_POST['txtEmail']);
-	$Password = mysqli_escape_string($con, $_POST['Password']);
-	$ConPassword = mysqli_escape_string($con, $_POST['ConPassword']);
+	$First_Name = mysqli_escape_string($conn, $_POST['txtFname']);
+	$Last_Name = mysqli_escape_string($conn, $_POST['txtLname']);
+	$Email = mysqli_escape_string($conn, $_POST['txtEmail']);
+	$Password = mysqli_escape_string($conn, $_POST['Password']);
+	$ConPassword = mysqli_escape_string($conn, $_POST['ConPassword']);
 	
 	$sql = "INSERT INTO publisher(Frist_Name, Last_Name, Email, Password) values('$First_Name','$Last_Name','$Email','$Password');";
 	
@@ -13,7 +13,7 @@
 		/*
 			Change this thing
 		*/
-		$con->query($sql);
+		$conn->query($sql);
 		
 		header("Location: ../PublisherLogin.php");
 	}
