5d443abd13792d60e0fbd536630487c8867e0046
Hasindu-Dilshan <it21436680@my.sliit.lk>
2021-10-17 18:46:48 +0530

    margin changed

--- indexs.php
+++ indexs.php
@@ -15,7 +15,7 @@ $ftab="";
 				
               $image = $rows['Cover_pic'];    
                 $ftab.=" <div style=' display: grid; grid-template-columns:auto ;'>";
-                $ftab.="<div class='card' style=' margin:3% 3% 3% 3%; padding: 3% 3% 3% 3%; '>";
+                $ftab.="<div class='card' style=' margin:2% 2% 2% 2%; padding: 2% 2% 2% 2%; '>";
                 $ftab.="<div class='row'> <div class='column'>";
                 $ftab.= "<center><img  class='apps'src='Src/it20029586/image/$image' width='10%' height='5%'></center><br>";
                /* $ftab.= "<h2> ". $rows['Book_Name']."</h2></br>";*/
